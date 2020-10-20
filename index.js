75be6ab1f7c66bb0a35adfbc44daefba1f90b91f
Joao Pedro <802.018@alunos.unigran.br>
2020-10-20 11:01:10 -0300

    perguntas em node

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+
+
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json()); 
+
+
+app.get("/",(req,res)=>{
+   res.render("index");
+});
+
+
+app.get("/perguntar",(req,res)=>{res.render("perguntar");});
+ 
+app.post("/salvarpergunta",(req,res)=>{
+      var titulo = req.body.titulo;
+      var desc = req.body.desc;
+   res.send("Formulario Recebido! titulo: " + titulo +" "+"descrição : "+ desc);
+});
+
+
+app.listen(8080,()=>{console.log("App rodando");});
